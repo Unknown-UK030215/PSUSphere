02274511e5f1cdccfc58e5e65c3303c743d75194
nanazo08 <gianajuanzo@gmail.com>
2025-04-02 22:30:01 +0800

    Modified Form

--- projectsite/studentorg/forms.py
+++ projectsite/studentorg/forms.py
@@ -1,8 +1,28 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Organization
+from .models import Organization, OrgMember, Student, College, Program
 
 class OrganizationForm(ModelForm):
     class meta:
         model = Organization
+        fields = "__all__"
+
+class OrgMemberForm(ModelForm):
+    class meta:
+        model = OrgMember
+        fields = "__all__"
+
+class StudentForm(ModelForm):
+    class meta:
+        model = Student
+        fields = "__all__"
+
+class CollegeForm(ModelForm):
+    class meta:
+        model = College
+        fields = "__all__"
+
+class ProgramForm(ModelForm):
+    class meta:
+        model = Program
         fields = "__all__"
